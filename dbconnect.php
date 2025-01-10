0cfc9fd772672fd62ce180c722d7cf89281288a1
Cheryl322 <cherylcheong88@gmail.com>
2025-01-10 14:45:01 +0800

    updated

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 //Set DB Parameter
 
 $servername ="localhost";
@@ -16,4 +16,25 @@ if (!$con) {
 
 mysqli_set_charset($con, "utf8mb4");
 
+?> -->
+
+<?php
+try {
+    $host = "localhost";
+    $dbname = "db_kada";
+    $username = "root";
+    $password = "";  // 如果有密码请填写
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 ?>
