cdf89289071fdfa7b5adaa8c7ace6ae952bf9cba
Cheryl322 <cherylcheong88@gmail.com>
2025-01-18 11:05:47 +0800

    updated cc

--- headermember.php
+++ headermember.php
@@ -37,6 +37,7 @@ if (isset($_SESSION['employeeID'])) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="css/bootstrap.min (1).css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <style>
