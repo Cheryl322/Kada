240b093a4b67511e6b8001db221078481c456654
Neo Li Xin <neolixin@gmail.com>
2025-01-18 22:14:54 +0800

    Updated

--- view_report_loan.php
+++ view_report_loan.php
@@ -21,7 +21,7 @@ if (!isset($_GET['download'])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body class="p-4">
-        <h3 class="text-center mb-4">Pengesahan Penyata Kewangan Ahli Koperasi Kakitangan KADA Kelantan Berhad</h3>
+        <h3 class="text-center mb-5">Pengesahan Penyata Kewangan Ahli Koperasi Kakitangan KADA Kelantan Berhad</h3>
         
         <div class="container">
             <h5 class="mb-3">Maklumat Peribadi</h5>
