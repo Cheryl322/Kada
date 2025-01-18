532510a5291b3e344e1f8ba709d5e2f03d3e0eaf
Neo Li Xin <neolixin@gmail.com>
2025-01-19 07:38:21 +0800

    Updated

--- view_report_loan.php
+++ view_report_loan.php
@@ -2,17 +2,56 @@
 session_start();
 require_once 'dbconnect.php';
 
-// If download parameter is not set, display HTML view
-if (!isset($_GET['download'])) {
-    $id = $_GET['id'];
-    $query = "SELECT m.*, l.* FROM tb_member m 
-              LEFT JOIN tb_loan l ON m.employeeID = l.employeeID 
-              WHERE m.employeeID = ?";
+// Get the loan application ID from URL
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+
+if ($id) {
+    // Modified query to get both loan and member details
+    $query = "SELECT 
+                m.memberName,
+                m.employeeID,
+                m.ic,
+                m.no_pf,
+                l.loanApplicationID,
+                l.loanID,
+                l.loanType,
+                l.amountRequested,
+                l.financingPeriod,
+                l.monthlyInstallments,
+                DATE_FORMAT(l.created_at, '%d/%m/%Y') as tarikh_pembiayaan
+              FROM tb_member m
+              INNER JOIN tb_loan l ON m.employeeID = l.employeeID
+              WHERE l.loanApplicationID = ?";
+              
     $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, 's', $id);
+    mysqli_stmt_bind_param($stmt, "s", $id);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $data = mysqli_fetch_assoc($result);
+    
+    // Check if data exists before accessing array values
+    if ($data) {
+        $nama = $data['memberName'];
+        $noAnggota = $data['employeeID'];
+        $noKadPengenalan = $data['ic'];
+        $noPF = $data['no_pf'];
+    } else {
+        // Set default values if no data found
+        $nama = '-';
+        $noAnggota = '-';
+        $noKadPengenalan = '-';
+        $noPF = '-';
+    }
+} else {
+    // Set default values if no ID provided
+    $nama = '-';
+    $noAnggota = '-';
+    $noKadPengenalan = '-';
+    $noPF = '-';
+}
+
+// If download parameter is not set, display HTML view
+if (!isset($_GET['download'])) {
     ?>
     <!DOCTYPE html>
     <html>
@@ -28,41 +67,51 @@ if (!isset($_GET['download'])) {
             <table class="table table-bordered">
                 <tr>
                     <th style="width: 30%">Nama</th>
-                    <td><?php echo htmlspecialchars($data['memberName']); ?></td>
+                    <td><?php echo htmlspecialchars($nama); ?></td>
                 </tr>
                 <tr>
-                    <th>No. Pekerja</th>
-                    <td><?php echo htmlspecialchars($data['employeeID']); ?></td>
+                    <th>No. Anggota</th>
+                    <td><?php echo htmlspecialchars($noAnggota); ?></td>
                 </tr>
                 <tr>
                     <th>No. Kad Pengenalan</th>
-                    <td><?php echo htmlspecialchars($data['ic']); ?></td>
+                    <td><?php echo htmlspecialchars($noKadPengenalan); ?></td>
                 </tr>
                 <tr>
                     <th>No. PF</th>
-                    <td><?php echo htmlspecialchars($data['no_pf']); ?></td>
+                    <td><?php echo htmlspecialchars($noPF); ?></td>
                 </tr>
             </table>
 
-            <h5 class="mt-4 mb-3">Maklumat Pembiayaan</h5>
-            <table class="table table-bordered">
-                <tr>
-                    <th style="width: 30%">Jenis Pembiayaan</th>
-                    <td><?php echo htmlspecialchars($data['loanType'] ?? '-'); ?></td>
-                </tr>
-                <tr>
-                    <th>Amaun Dipohon</th>
-                    <td>RM <?php echo number_format($data['amountRequested'] ?? 0, 2); ?></td>
-                </tr>
-                <tr>
-                    <th>Tempoh Pembiayaan</th>
-                    <td><?php echo htmlspecialchars($data['financingPeriod'] ?? '-'); ?> bulan</td>
-                </tr>
-                <tr>
-                    <th>Ansuran Bulanan</th>
-                    <td>RM <?php echo number_format($data['monthlyInstallments'] ?? 0, 2); ?></td>
-                </tr>
-            </table>
+            <?php if ($data) { ?>
+                <h5 class="mt-4 mb-3">Maklumat Pembiayaan</h5>
+                <table class="table table-bordered">
+                    <tr>
+                        <th style="width: 30%">No. Pembiayaan</th>
+                        <td><?php echo htmlspecialchars($data['loanID']); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Jenis Pembiayaan</th>
+                        <td><?php echo htmlspecialchars($data['loanType']); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Amaun Dipohon</th>
+                        <td>RM <?php echo number_format($data['amountRequested'], 2); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Tempoh Pembiayaan</th>
+                        <td><?php echo htmlspecialchars($data['financingPeriod']); ?> bulan</td>
+                    </tr>
+                    <tr>
+                        <th>Ansuran Bulanan</th>
+                        <td>RM <?php echo number_format($data['monthlyInstallments'], 2); ?></td>
+                    </tr>
+                    <tr>
+                        <th>Tarikh Pembiayaan</th>
+                        <td><?php echo htmlspecialchars($data['tarikh_pembiayaan']); ?></td>
+                    </tr>
+                </table>
+            <?php } ?>
             
             <div class="text-muted mt-3">
                 <small>Laporan dijana pada: <?php echo date('d/m/Y H:i:s'); ?></small>
@@ -71,71 +120,5 @@ if (!isset($_GET['download'])) {
     </body>
     </html>
     <?php
-    exit();
-} else {
-    // PDF generation code
-    $id = $_GET['id'];
-    $query = "SELECT m.*, l.* FROM tb_member m 
-              LEFT JOIN tb_loan l ON m.employeeID = l.employeeID 
-              WHERE m.employeeID = ?";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, 's', $id);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-    $data = mysqli_fetch_assoc($result);
-
-    // Your existing PDF generation code
-    // Title
-    $pdf->SetFont('helvetica', 'B', 14);
-    $pdf->Cell(0, 10, 'Pengesahan Penyata Kewangan Ahli Koperasi', 0, 1, 'C');
-    $pdf->Cell(0, 10, 'Kakitangan KADA Kelantan Berhad', 0, 1, 'C');
-    $pdf->Ln(10);
-
-    // Member details
-    $pdf->SetFont('helvetica', '', 12);
-    $pdf->SetFillColor(245, 245, 245);
-    $pdf->SetDrawColor(200, 200, 200);
-
-    // Personal details section
-    $pdf->Cell(60, 10, 'Nama', 1, 0, 'L', true);
-    $pdf->Cell(130, 10, $data['memberName'], 1, 1, 'L', true);
-    
-    $pdf->Cell(60, 10, 'No. Anggota', 1, 0, 'L', true);
-    $pdf->Cell(130, 10, $data['employeeID'], 1, 1, 'L', true);
-    
-    $pdf->Cell(60, 10, 'No. Kad Pengenalan', 1, 0, 'L', true);
-    $pdf->Cell(130, 10, $data['ic'], 1, 1, 'L', true);
-    
-    $pdf->Cell(60, 10, 'No. PF', 1, 0, 'L', true);
-    $pdf->Cell(130, 10, $data['no_pf'], 1, 1, 'L', true);
-
-    $pdf->Ln(10);
-
-    // Loan details section
-    $pdf->SetFont('helvetica', 'B', 12);
-    $pdf->Cell(0, 10, 'Maklumat Pembiayaan', 0, 1, 'L');
-    $pdf->SetFont('helvetica', '', 12);
-
-    $pdf->Cell(60, 10, 'Jenis Pembiayaan', 1, 0, 'L', true);
-    $pdf->Cell(130, 10, $data['loanType'] ?? '-', 1, 1, 'L', true);
-    
-    $pdf->Cell(60, 10, 'Amaun Dipohon', 1, 0, 'L', true);
-    $pdf->Cell(130, 10, 'RM ' . number_format($data['amountRequested'] ?? 0, 2), 1, 1, 'L', true);
-    
-    $pdf->Cell(60, 10, 'Tempoh Pembiayaan', 1, 0, 'L', true);
-    $pdf->Cell(130, 10, ($data['financingPeriod'] ?? '-') . ' bulan', 1, 1, 'L', true);
-    
-    $pdf->Cell(60, 10, 'Ansuran Bulanan', 1, 0, 'L', true);
-    $pdf->Cell(130, 10, 'RM ' . number_format($data['monthlyInstallments'] ?? 0, 2), 1, 1, 'L', true);
-
-    // Add timestamp at the bottom
-    $pdf->Ln(10);
-    $pdf->SetFont('helvetica', 'I', 10);
-    $pdf->Cell(0, 10, 'Laporan dijana pada: ' . date('d/m/Y H:i:s'), 0, 1, 'L');
-
-    // Output the PDF
-    $pdf->Output('financial_statement_' . $id . '.pdf', 'D');
-    exit();
 }
-
-// Rest of your view logic for normal viewing... 
+?>
